d94acee62a73f495ac7da584fe16c04b3b7ec7bb
Nguyen Quang Do <quangdo0206@gmail.com>
2025-07-18 12:52:08 +0700

    feat: update dependencies in package.json and package-lock.json; remove unused imports in MyTrip component

--- src/pages/MyTrip/index.js
+++ src/pages/MyTrip/index.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { notification } from 'antd';
-import { AuthContext } from '~/components/Context/auth.context';
+import React, { useState, useEffect } from 'react';
+
 import classNames from 'classnames/bind';
 import { motion } from 'framer-motion';
 import { Spin, message, Empty } from 'antd';
